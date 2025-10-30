f04eadb97891072cb2763386cf7cd22c0f15d653
Zack <jesus34canul@gmail.com>
2025-10-29 19:29:49 -0600

    2.1.20

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -9,4 +9,16 @@ const serverConfig: ApplicationConfig = {
   ]
 };
 
+// 👇 Esta es la configuración final que usas en main.server.ts
 export const config = mergeApplicationConfig(appConfig, serverConfig);
+
+// 👇 Esta función le dice a Angular qué rutas prerenderar
+export function getPrerenderRoutes() {
+  return [
+    '/', '/login', '/inicio', '/registro',
+    '/datos-medicos1', '/historia-clinica1',
+    '/navegacion', '/tabla-medica', '/antecedentes-h',
+    '/qrp'
+    // ❌ NO incluyas '/acceso/:token'
+  ];
+}
