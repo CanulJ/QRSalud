9d390561fa750c084e0efba4955402403e300d2f
Zack <jesus34canul@gmail.com>
2025-10-29 20:05:36 -0600

    2.1.24

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -45,4 +45,12 @@ export const routes: Routes = [
   { path: 'qrp/id', component: QRP },
 
   { path: 'acceso/:token', component: QRP },
+
+  {
+  path: 'acceso/:token',
+  loadComponent: () =>
+    import('./Pages/inicio/inicio').then(m => m.Inicio),
+  data: { renderMode: 'client' } // 👈 evita que Netlify intente prerenderizarla
+}
+
 ];
